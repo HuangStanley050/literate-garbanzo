b88ebd5290e8954ed06924d220a6c499b5614436
Stanley Huang <huang.stanley050@gmail.com>
2019-12-28 10:55:23 +1100

    able to save user to database after google oAuth

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,6 +2,8 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const User = require("../models/User");
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.JWT_SECRET;
@@ -16,12 +18,24 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: `/auth/google/callback`
     },
-    (accessToken, refreshToken, profile, done) => {
-      const googleID = profile.id;
-      const email = profile.emails[0].value;
-      const name = profile.displayName;
-      const userInfo = { googleID, email, name };
-      return done(null, userInfo);
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const googleID = profile.id;
+        const email = profile.emails[0].value;
+        const name = profile.displayName;
+        const userInfo = { googleID, email, name };
+
+        const existingUser = await User.findOne({ googleID: profile.id });
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+        const user = await new User({
+          ...userInfo
+        }).save();
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
     }
   )
 );
--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = Schema({});
+const userSchema = Schema({
+  googleID: String,
+  email: String,
+  name: String
+});
 
 module.exports = mongoose.model("User", userSchema);
