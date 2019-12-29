0c0fffc032e10f1a692fca8dde08a59c141d5f88
Stanley Huang <huang.stanley050@gmail.com>
2019-12-29 17:43:13 +1100

    added link to api /auth/google and link to landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+import Container from "@material-ui/core/Container";
 import Landing from "./components/Landing";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
 import SurveyNew from "./components/SurveyNew";
 
 function App() {
+  useEffect(() => {
+    console.log(window.location.search);
+  }, []);
   return (
-    <div>
+    <Container maxWidth="lg">
       <Header />
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/surveys" component={Dashboard} />
         <Route path="/surveys/new" component={SurveyNew} />
       </Switch>
-    </div>
+    </Container>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,6 +6,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
+import { Link as RouterLink } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -27,6 +28,8 @@ const Header = () => {
       <AppBar position="static">
         <Toolbar>
           <IconButton
+            component={RouterLink}
+            to="/"
             edge="start"
             className={classes.menuButton}
             color="inherit"
@@ -34,10 +37,13 @@ const Header = () => {
           >
             <AssignmentIndIcon />
           </IconButton>
+
           <Typography variant="h6" className={classes.title}>
             SurveyApp
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" href="/auth/google">
+            Login with Google
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
