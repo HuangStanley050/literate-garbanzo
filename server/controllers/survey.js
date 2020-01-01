b482b69657ffc0b8a0faf20c5c0efd6a25042cb0
Stanley Huang <huang.stanley050@gmail.com>
2020-01-01 22:28:48 +1100

    made sure the User and Survey relationship links up after saving survey

--- server/controllers/survey.js
+++ server/controllers/survey.js
@@ -1,17 +1,21 @@
 const Survey = require("../models/Survey");
+const User = require("../models/User");
 exports.fetchSurveys = (req, res, next) => {};
 exports.createSurvey = async (req, res, next) => {
   const { id } = req.user;
   const { title, subject, body, recipients } = req.body;
-
   const newSurvey = new Survey({
     title,
     subject,
     body,
+    dateSent: Date.now(),
     recipients: recipients.split(",").map(email => ({ email })),
     user: id
   });
-  await newSurvey.save();
+  const user = await User.findOne({ _id: id });
+  const surveyResult = await newSurvey.save();
+  user.surveys = [...user.surveys, surveyResult._id];
+  await user.save();
   res.send("this is create survey route");
 };
 exports.webHook = (req, res, next) => {};
