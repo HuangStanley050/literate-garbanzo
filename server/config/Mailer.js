53dc571c35ac325b3d07f7362b604d8fdd48cace
Stanley Huang <huang.stanley050@gmail.com>
2020-01-02 20:21:55 +1100

    finished Mailer class

--- server/config/Mailer.js
+++ server/config/Mailer.js
@@ -2,9 +2,21 @@ const sgMail = require("@sendgrid/mail");
 const helper = sgMail.mail;
 
 class Mailer {
-  constructor() {}
+  constructor({ subject, recipients }, content) {
+    sgMail.setApiKey(process.env.SENDGRID_KEY);
+    this.msg = {
+      to: recipients.map(({ email }) => email),
+      from: "no-reply@surveyApp.com",
+      subject,
+      html: content,
+      trackingSettings: { enable_text: true, enabled: true }
+    };
+  }
 
-  async send() {}
+  async send() {
+    const response = await sgMail.send(this.msg);
+    return response;
+  }
 }
 
 module.exports = Mailer;
--- server/controllers/survey.js
+++ server/controllers/survey.js
@@ -1,5 +1,8 @@
 const Survey = require("../models/Survey");
+const Mailer = require("../config/Mailer");
+const surveyTemplate = require("../config/emailTemplates");
 const User = require("../models/User");
+
 exports.fetchSurveys = (req, res, next) => {};
 exports.createSurvey = async (req, res, next) => {
   const { id } = req.user;
@@ -12,10 +15,11 @@ exports.createSurvey = async (req, res, next) => {
     recipients: recipients.split(",").map(email => ({ email: email.trim() })),
     user: id
   });
-  const user = await User.findOne({ _id: id });
-  const surveyResult = await newSurvey.save();
-  user.surveys = [...user.surveys, surveyResult._id];
-  await user.save();
-  res.send("this is create survey route");
+  const mailer = new Mailer(survey, surveyTemplate(survey));
+  // const user = await User.findOne({ _id: id });
+  // const surveyResult = await newSurvey.save();
+  // user.surveys = [...user.surveys, surveyResult._id];
+  // await user.save();
+  // res.send("this is create survey route");
 };
 exports.webHook = (req, res, next) => {};
