f1f38dab7d4ff629da1e8153521fbcb123ec58df
Stanley Huang <huang.stanley050@gmail.com>
2020-01-04 14:41:44 +1100

    added fab button to form

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,23 @@
 import React from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
 
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  const fabStyle = {
+    margin: 0,
+    top: "auto",
+    right: 20,
+    bottom: 20,
+    left: "auto",
+    position: "absolute"
+  };
+  return (
+    <div style={{ position: "relative", minHeight: "90vh" }}>
+      <Fab style={fabStyle} color="secondary" aria-label="add">
+        <AddIcon />
+      </Fab>
+    </div>
+  );
 };
 
 export default Dashboard;
