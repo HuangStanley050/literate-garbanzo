f48a8df390806f42df2852075d035c121e519e55
Stanley Huang <huang.stanley050@gmail.com>
2020-01-05 20:39:01 +1100

    able to get the form values in the review form page

--- client/src/components/SurveyFormReview.js
+++ client/src/components/SurveyFormReview.js
@@ -3,13 +3,19 @@ import Button from "@material-ui/core/Button";
 import { reduxForm } from "redux-form";
 
 const Review = props => {
+  // console.log("from form review");
+  // console.log(props.formFields);
   return (
     <div>
       <h1>Review</h1>
       <Button onClick={props.prevPage} variant="contained" color="primary">
         Go back
       </Button>
-      <Button variant="contained" color="secondary">
+      <Button
+        onClick={props.handleSubmit}
+        variant="contained"
+        color="secondary"
+      >
         Send Survey
       </Button>
     </div>
--- client/src/components/SurveyNew.js
+++ client/src/components/SurveyNew.js
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
 import SurveyForm from "./SurveyForm";
 import SurveyFormReview from "./SurveyFormReview";
+import { connect } from "react-redux";
 
-const SurveyNew = () => {
+const SurveyNew = props => {
   const [currentPage, setPage] = useState(1);
+  const handleSubmit = () => {
+    console.log(props.form);
+  };
   const nextPage = () => setPage(currentPage + 1);
   const prevPage = () => setPage(currentPage - 1);
   return (
     <div>
       {currentPage === 1 && <SurveyForm onSubmit={nextPage} />}
-      {currentPage === 2 && <SurveyFormReview prevPage={prevPage} />}
+      {currentPage === 2 && (
+        <SurveyFormReview
+          formFields={props.form.values || []}
+          prevPage={prevPage}
+          handleSubmit={handleSubmit}
+        />
+      )}
     </div>
   );
 };
-
-export default SurveyNew;
+const mapState = state => ({
+  form: state.form.surveyForm
+});
+export default connect(mapState)(SurveyNew);
