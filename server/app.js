910488239bb6d822d14d0f4d1c604bc551589084
Stanley Huang <huang.stanley050@gmail.com>
2020-01-09 20:40:31 +1100

    issue with mongo string

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 require("./config/passport");
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(() => console.log("connected to database"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
